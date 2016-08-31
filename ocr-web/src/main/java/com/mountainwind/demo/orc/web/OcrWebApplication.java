d9407964f5f5c257254deb2b1c5b98fcccccf05f
peijie <peijie_wang@hotmail.com>
2016-08-31 23:48:33 +0800

    added some potential beans in sample_rabbitmq

--- ocr-web/src/main/java/com/mountainwind/demo/orc/web/OcrWebApplication.java
+++ ocr-web/src/main/java/com/mountainwind/demo/orc/web/OcrWebApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-public class OcrWebApplication {
-
+public class OcrWebApplication  {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OcrWebApplication.class, args);
 	}
+
 }
