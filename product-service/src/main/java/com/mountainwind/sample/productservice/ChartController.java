93de1871e457893de13afff2437f178c40f24a76
peijie <peijie_wang@hotmail.com>
2016-09-19 00:24:03 +0800

    clean repository

--- product-service/src/main/java/com/mountainwind/sample/productservice/ChartController.java
+++ product-service/src/main/java/com/mountainwind/sample/productservice/ChartController.java
@@ -1,8 +1,5 @@
 package com.mountainwind.sample.productservice;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChartController {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<ChartData> findAll() {
-		List<ChartData> chart = new ArrayList<ChartData>();
-		chart.add(new ChartData("2004", 1000, 400));
-		chart.add(new ChartData("2005", 1170, 460));
-		chart.add(new ChartData("2006", 660, 1120));
-		chart.add(new ChartData("2007", 1030, 540));
+	public String[][] findAll() {
+		String[][] chart = new String[6][3];
+		chart[0] = new String[] {"2004", "1000", "400"};
+		chart[1] = new String[] {"2005", "1170", "460"};
+		chart[2] = new String[] {"2006", "660", "1120"};
+		chart[3] = new String[] {"2007", "1030", "540"};
+		chart[4] = new String[] {"2008", "1400", "700"};
+		chart[5] = new String[] {"2009", "800", "1000"};
+		
+//		List<ChartData> chart = new ArrayList<ChartData>();
+//		chart.add(new ChartData("2004", 1000, 400));
+//		chart.add(new ChartData("2005", 1170, 460));
+//		chart.add(new ChartData("2006", 660, 1120));
+//		chart.add(new ChartData("2007", 1030, 540));
 		
 		return chart;
 	}
